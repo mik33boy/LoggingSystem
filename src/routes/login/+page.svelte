50fc355cb1625ddad2ec50b926a218e752307a7b
Michael <mdd23300332@gmail.com>
2025-05-16 15:54:15 +0800

    update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -9,10 +9,6 @@
     // Implement your login logic here
   }
 
-  function togglePasswordVisibility() {
-    showPassword = !showPassword; // Toggle the visibility of the password
-  }
-
   // Reactive statement to determine the input type
   $: inputType = showPassword ? 'text' : 'password';
 </script>
@@ -73,25 +69,8 @@
                 required
                 class="w-full pl-10 pr-10 py-4 border border-gray-200 rounded-md text-gray-500 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"
                 placeholder="Enter Password"
+                type="password"
               />
-              <!-- Toggle password visibility button -->
-              <button 
-                type="button" 
-                class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400"
-                on:click={togglePasswordVisibility}
-              >
-                {#if showPassword}
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                  </svg>
-                {:else}
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
-                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                  </svg>
-                {/if}
-              </button>
             </div>
           </div>
         </div>
